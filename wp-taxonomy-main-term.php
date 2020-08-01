32a0cfef0751ed97c3937d0c122d78a416b5cab4
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-08-01 19:19:11 -0300

    improve plugin description

--- wp-taxonomy-main-term.php
+++ wp-taxonomy-main-term.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WP Taxonomy Main Term
-Description: Plugin to select a primary (main) taxonomy term for a post.
+Description: Select a primary (main) term for every post taxonomy.
 Author: Nicolas Menescardi
 Version: 0.0.1
 Text Domain: wp-taxonomy-main-term
@@ -11,4 +11,3 @@ require_once 'vendor/autoload.php';
 require_once 'Base.php';
 
 add_action('plugins_loaded', array(new WP_TMT\Base, 'load'));
-
